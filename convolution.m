cb92099c6766fea04dbe82d4851bb8baafd5297e
SUBHRANIL MUSTAFI <subhranilmustafi2011@gmail.com>
2025-05-05 01:27:27 +0530

    Update convolution.m

--- convolution.m
+++ convolution.m
@@ -1,28 +1,30 @@
 function output_post_conv = convolution(output_pre_convolution, dimdata)
-output_post_conv = zeros(4279,6826,dimdata);
-nhood = zeros(4279,6826,dimdata);
-for i=1:dimdata
-    nhood(:,:,i) = output_pre_convolution(:,:,i);
+    % Extract the raster dimensions from input
+    [r, c, ~] = size(output_pre_convolution);
+    
+    % Initialize outputs dynamically
+    output_post_conv = zeros(r, c, dimdata);
+    nhood = zeros(r, c, dimdata);
+
+    for i = 1:dimdata
+        nhood(:, :, i) = output_pre_convolution(:, :, i);
+    end
+
+    % Define the convolution kernel
+    k_size = 3;
+    kernel = ones(k_size, k_size);
+    kernel(ceil(k_size / 2), ceil(k_size / 2)) = 0;
+
+    % Apply convolution and thresholding
+    for idx = 1:size(nhood, 3)
+        currentNhood = nhood(:, :, idx);
+        convolutionResult = convn(currentNhood, kernel, 'same');
+        
+        % Threshold value
+        thresholdValue = 4;
+        currentNhood(convolutionResult > thresholdValue) = 1;
+        currentNhood(convolutionResult <= thresholdValue) = 0;
+        
+        output_post_conv(:, :, idx) = currentNhood;
+    end
 end
-
-k_size = 3;
-kernel = ones(k_size, k_size);
-kernel((k_size + 1) / 2, (k_size + 1) / 2) = 0;
-
-% Iterate over nhood matrices
-for idx = 1:size(nhood, 3)
-    currentNhood = nhood(:, :, idx);
-
-    % Perform convolution
-    convolutionResult = convn(currentNhood, kernel, 'same');
-
-    % Thresholding based on the convolution result
-    thresholdValue = 4;
-    currentNhood(convolutionResult > thresholdValue) = 1;
-    currentNhood(convolutionResult <= thresholdValue) = 0;
-
-    % Update the nhood matrix
-    output_post_conv(:, :, idx) = currentNhood;
-end
-end
-  
